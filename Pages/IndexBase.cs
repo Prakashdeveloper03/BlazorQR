5d69f168283f4dc6543593e6f134d31f808bf183
Siva Prakash <prakashdeveloper@outlook.in>
2023-04-20 10:19:32 +0530

    Added IndexBase.cs & Updated Index.razor file

---
+++ Pages/IndexBase.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace QRCodeGenerator.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected string wrapperClass => "wrapper";
+    }
+}
